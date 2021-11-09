e976a78b0e2fbedea77086875354e9caea9b2aba
muratcelikk <muratcelik11@gmail.com>
2021-11-09 15:56:01 +0300

    via

---
+++ src/main/java/com/mrtclk/controller/MainController.java
@@ -0,0 +1,37 @@
+package com.mrtclk.controller;
+
+import com.mrtclk.model.Person;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class MainController {
+
+    private static List<Person> persons = new ArrayList<>();
+
+    static {
+        persons.add(new Person("Murat", "Celik"));
+        persons.add(new Person("Fırat", "Celik"));
+        persons.add(new Person("Mesut", "Celik"));
+    }
+
+    @Value("${welcome.message}")
+    private String message;
+
+    @RequestMapping(value = {"/", "/index"}, method = RequestMethod.GET)
+    private String index(Model model) {
+        model.addAttribute("message", message);
+        return "index";
+    }
+    @RequestMapping(value = {"/personList"}, method = RequestMethod.GET)
+    private String personList(Model model){
+        model.addAttribute("persons", persons);
+        return "personList";
+    }
+}
